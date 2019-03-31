f3d31655c4c923b356dc7c050c0291401d6981ac
SWrinn <35043165+SWrinn@users.noreply.github.com>
2019-03-30 21:53:09 -0400

    Added session information
    Gets user information based on the session.
    Also removed accuracy and wpm options.

--- Profile/firebaseStuff.js
+++ Profile/firebaseStuff.js
@@ -1,61 +1,84 @@
-var database = firebase.database();
-var user = firebase.auth().currentUser; //this will be null if there isn't anyone currently logged in
+var user;
 var points, username, imgURL;
-
-function signOut() {
-    // Sign out of Firebase.
-    firebase.auth().signOut()
-        .then(function() {
-            //redirect to home page, maybe??
-            window.location = "./index.html";
-        }).catch(function(error) {
-            //there was an error when logging out
-            alert('An error occured when trying to log out');
-        });
-}
-
-function inputInfo() {
-    //input the information where it belongs
-    if (user) {
-        points = user.points;
-        username = user.username;
-        imgURL = user.imgURL;
-
-        document.getElementById("username").innerHTML = username;
-
-        document.getElementById("pts").innerHTML = points;
-        document.getElementById("profPic").src = imgURL;
-    } else {
-        alert('You are not logged in!');
-    }
-}
 
 window.onload = inputInfo;
 
-function changePic(id) {
-    if (user) {
-        user.updateProfile({
-            imgURL: "" + document.getElementById(id).src
-        }).then(function() {
-            //successful update
-            imgURL = user.imgURL;
-            document.getElementById("profPic").src = imgURL;
-        }).catch(function(error) {
-            alert('An error occured when updating');
-        });
-    } else {
-        document.getElementById("profPic").src = document.getElementById(id).src;
-    }
+function signOut(){
+  // Sign out of Firebase.
+  firebase.auth().signOut()
+    .then(function(){
+      //redirect to home page, maybe??
+      if(typeof(Storage) !== "undefined"){
+        //reset the session, so that the user's info isn't there anymore
+        sessionStorage.user = "";
+      }
+      window.location = "./index.html";
+    }).catch(function(error){
+      //there was an error when logging out
+      alert('An error occured when trying to log out');
+    });
 }
 
-function changePass() {
-    if (user) {
-        firebase.auth().sendPasswordResetEmail(user.email).then(function() {
-            //successfully sent email
-        }).catch(function(error) {
-            //there was an error when sending the email
-        });
-    } else {
-        alert("You are not logged in");
+function inputInfo(){
+  //input the information where it belongs
+  if(typeof(Storage) !== "undefined"){
+    if(sessionStorage.user){
+      user = sessionStorage.user;
+    }else{
+      //this can probably get deleted later on
+      alert('no session');
     }
+}
+  if(user){
+    points = user.points;
+    username = user.username;
+    imgURL = user.photoURL;
+
+    //document.getElementById("username").innerHTML = username;
+
+    //document.getElementById("pts").innerHTML = points;
+
+    //document.getElementById("profPic").src = imgURL;
+
+    //once the database has been configured, this will be changed
+
+    document.getElementById("username").innerHTML = "username";
+    document.getElementById("pts").innerHTML = "points";
+    document.getElementById("profPic").src = "http://i.imgur.com/YdhUZdZ.png";
+  }else{
+    alert('You are not logged in!');
+  } 
+}
+
+function changePic(id){
+  //this is currently under construction
+  if(user){
+    user.updateProfile({
+      photoURL: "" + document.getElementById(id).src
+    }).then(function(){
+      //successful update
+      imgURL = user.photoURL;
+      document.getElementById("profPic").src = imgURL;
+    }).catch(function(error){
+      alert('An error occured when updating');
+    });
+  }else{
+    //document.getElementById("profPic").src = document.getElementById(id).src;
+    alert('You are not logged in!');
+  }
+}
+
+function changePass(){
+  //this too is under construction
+  if(user){
+    firebase.auth().sendPasswordResetEmail(user.email).then(function(){
+      //successfully sent email
+      alert('Email sent successfully!');
+    }).catch(function(error){
+      //there was an error when sending the email
+      alert('There was a problem in sending the email');
+    });
+  }else{
+    alert("You are not logged in");
+  }
 }
